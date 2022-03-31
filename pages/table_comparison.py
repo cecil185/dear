3d08c43b41d0ecbe388556103a0ba4c7abea12f1
Cecil <cecil@modern.inc>
2022-03-30 21:50:56 -0300

    Gets new columns and rows names

--- pages/table_comparison.py
+++ pages/table_comparison.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 import pandas as pd
+from functions.dataset_ops import getElementsNotSharedInLists
 
 def app():
 
@@ -12,6 +13,16 @@ def app():
     c1, c2 = st.columns(column_widths)
     c1.subheader("Before")
     c2.subheader("After")
+
+    # If flags are already initialized, don't do anything
+    if 'f1' not in st.session_state:
+        st.session_state.f1 = False
+    if 'f2' not in st.session_state:
+        st.session_state.f2 = False
+    if 'df_before' not in st.session_state:
+        st.session_state.df_before = None
+    if 'df_after' not in st.session_state:
+        st.session_state.df_after = None
 
 ## Upload before data ##
     data_before = c1.file_uploader("Upload data before changes", type=["csv", "xlsx"])    
@@ -28,8 +39,9 @@ def app():
             c1.markdown("Please select a file to upload first")
         else:
             # Save uploaded CSV as dataframe
-            df_before = data1
+            st.session_state.df_before = data1
             c1.markdown("Load Complete")
+            st.session_state.f1=True
         
 
 ## Upload after data ##
@@ -48,21 +60,50 @@ def app():
             c2.markdown("Please select a file to upload first")
         else:
             # Save uploaded CSV as dataframe
-            df_after = data2
+            st.session_state.df_after = data2
             c2.markdown("Load Complete")
-        
-            # Multiselect for identifying keys
-            keys = st.multiselect("Which column(s) are unique keys? (must be the same for both datasets)", df_after.columns)
-            if len(keys) > 0:
-                st.sidebar.markdown("hello")
+            st.session_state.f2=True
 
-        
 
-## Three dividers ##
-    st.markdown("---")
+    if st.session_state.f1 and st.session_state.f2:
+        # Multiselect for identifying keys
+        keys = st.multiselect("Which column(s) are unique keys? (must be the same for both datasets)", st.session_state.df_after.columns)
+        if len(keys) > 0:
+            #initialize key columns
+            st.session_state.df_before['key']=''
+            st.session_state.df_after['key']=''
+            
+            #create new column key which combines all key columns
+            for k in keys:
+                st.session_state.df_before['key']+=st.session_state.df_before[k].astype(str)
+                st.session_state.df_after['key']+=st.session_state.df_after[k].astype(str)
+            
+            #if unique combo is not unique there will be a problem
 
-    new_rows_expander = st.expander(label="New Rows", expanded=False)
+            #gets new and deleted rows
+            list1=st.session_state.df_before['key']
+            list2=st.session_state.df_after['key']
+            new_rows_list, deleted_rows_list = getElementsNotSharedInLists(list1, list2)
 
-    deleted_rows_expander = st.expander(label="Deleted Rows", expanded=False)
+            st.write(new_rows_list)
+            st.write(deleted_rows_list)
+            
+            #gets new and deleted columns
+            list1=st.session_state.df_before.columns
+            list2=st.session_state.df_after.columns
+            new_cols_list, deleted_cols_list = getElementsNotSharedInLists(list1, list2)
 
-    changed_rows_expander = st.expander(label="Modified Rows", expanded=False)
+            st.write(new_cols_list)
+            st.write(deleted_cols_list)
+
+        ## Three dividers ##
+        st.markdown("---")
+
+        new_rows_expander = st.expander(label="New Rows", expanded=False)
+        #with new_rows_expander:
+            
+        deleted_rows_expander = st.expander(label="Deleted Rows", expanded=False)
+        #with deleted_rows_expander:
+            
+        changed_rows_expander = st.expander(label="Modified Rows", expanded=False)
+
